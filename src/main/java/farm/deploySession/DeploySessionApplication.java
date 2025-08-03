461f7d45a6dfa44d0fa298aee6c588a9dd2f9c4c
saokiritoni <soeun.love.lee@gmail.com>
2025-08-03 14:08:41 +0900

    [init] settings

---
+++ src/main/java/farm/deploySession/DeploySessionApplication.java
@@ -0,0 +1,13 @@
+package farm.deploySession;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeploySessionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeploySessionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/farm/deploySession/DeploySessionApplicationTests.java
@@ -0,0 +1,13 @@
+package farm.deploySession;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeploySessionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
